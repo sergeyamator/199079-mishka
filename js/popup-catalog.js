f2827d85c09d3e581fe2a96f70f1f4e7dbb91884
Lena Gritsuk <lenagritsuk@inbox.ru>
2016-12-22 18:17:34 +0300

    popup

--- js/popup-catalog.js
+++ js/popup-catalog.js
@@ -1,24 +1,38 @@
-    var link_cart = document.querySelector(".catalog-item__order-btn");
-    var popup = document.querySelector(".modal-form");
-    var overlay = document.querySelector(".modal-overlay");
+var catalog = document.querySelector(".catalog__wrapper");
+var popup = document.querySelector(".modal-form");
+var overlay = document.querySelector(".modal-overlay");
 
-    link_cart.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.add("modal-form-show");
-      overlay.classList.add("modal-overlay-show");
-    });
+catalog.onclick = function(event) {
+  var target = event.target;
+  while (target != catalog) {
+     if (!target.classList) {
+      continue;
+    }
+    if (target.classList.contains("catalog-item__order-btn")) {
+      console.log(target);
+      modal_form_show(event)
+      return;
+    }
+    target = target.parentNode;
+  }
+}
+function modal_form_show(event) {
+  event.preventDefault();
+  popup.classList.add("modal-form-show");
+  overlay.classList.add("modal-overlay-show");
+};
 
-    overlay.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.remove("modal-form-show");
-      overlay.classList.remove("modal-overlay-show");
-    });
+overlay.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-form-show");
+  overlay.classList.remove("modal-overlay-show");
+});
 
-    window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (popup.classList.contains("modal-form-show")) {
-            popup.classList.remove("modal-form-show");
-            overlay.classList.remove("modal-overlay-show");
-          }
-        }
-      });
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (popup.classList.contains("modal-form-show")) {
+        popup.classList.remove("modal-form-show");
+        overlay.classList.remove("modal-overlay-show");
+      }
+    }
+  });
